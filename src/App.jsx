fb6cc714c0ef03efc0203b76287dbc3619a35b1e
aveandrian <trancemilly7@gmail.com>
2023-06-20 22:41:34 +0200

    adds editing and replying

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,59 @@
 import { useState } from 'react'
 import Comment from './components/Comment'
+import { nanoid } from 'nanoid'
 import data from './data.json'
 import './App.css'
 
 function App() {
-  const currentUser = useState(data.currentUser)
+  const currentUser = data.currentUser
   const [comments, setComments] = useState(data.comments)
+
+  function handleReply(parentId, content, toUser){   
+    console.log("To user", toUser)
+    setComments(comments => comments.map(comment => {
+      return comment.id ==  parentId ? {...comment, replies: [...comment.replies, 
+        {
+          id: nanoid(),
+          content: content,
+          score: 0,
+          replyingTo: toUser,
+          user: currentUser,
+          createdAt: 'now'
+        }
+      ]} : comment
+    }))
+  }
+
+  function updateComment(id, content, isReply, parentId){
+    console.log(id, content, isReply, parentId)
+    isReply ? 
+    setComments(comments => comments.map(comment => {
+      return comment.id == parentId ? 
+      {...comment, replies: comment.replies.map(reply => {
+        return reply.id == id ? {...reply, content: content} : reply
+      })}
+      : comment
+    })) 
+    : setComments(comments => comments.map(comment => {
+      return comment.id == id ? {...comment, content: content} : comment
+    }))
+  }
+
+  console.log(comments)
   return (
     <>
-      <Comment />
-      <div class="attribution">
+      <div className='comments-section'>
+        {comments.map(comment => <Comment 
+          currentUser={currentUser} 
+          isReply={false} 
+          key={comment.id} 
+          {...comment} 
+          parentId={comment.id} 
+          handleReply={handleReply}
+          updateComment={updateComment}
+        />)}
+      </div>
+      <div className="attribution">
         Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
         Coded by <a href="#">Your Name Here</a>.
       </div>
--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,37 +1,96 @@
+import { useState } from 'react'
 import '../styles/Comment.css'
 
-export default function Comment(){
+export default function Comment(props){
+    console.log(props)
+    const [isEditing, setIsEditing] = useState(false)
+    const [isReplying, setisReplying] = useState(false)
+    const [commentTextValue, setCommentTextValue] = useState('@'+props.replyingTo + " " + props.content)
+    const [replyingTextValue, setReplyingTextValue] = useState("")
+
+    function handleReplyingTextValueChange(e){
+        setReplyingTextValue(e.target.value)
+    }
+
+    function toggleisReplying(){
+        setReplyingTextValue('@'+ props.user.username + " ")
+        setisReplying(prev=>!prev)
+    }
+
+    function toggleIsEditing(){
+        if(props.content != commentTextValue){
+            setCommentTextValue(props.content)
+        }
+        setIsEditing(prev => !prev)
+    }
+
+    function handleChange(e){
+        setCommentTextValue(e.target.value)
+    }
     return (
+        <>
         <div className="comment">
-            <div className="comment-score">
-                <img src="/images/icon-plus.svg" alt="" />
-                <p className='comment-score-text'>12</p>
-                <img src="/images/icon-minus.svg" alt="" />
-            </div>
-            <div className="comment-content">
-                <div className="comment-header">
-                    <img className="comment-avatar" src="/images/avatars/image-amyrobson.png"/>
-                    <p className="comment-author">amyrobson</p>
-                    <p className="comment-time">1 month ago</p>
-                    <div className="comment-actions">
-                        <div className="delete-action">
-                            <img className="delete-icon" src='/images/icon-delete.svg' />
-                            <p className="delete-text">Delete</p>
-                        </div>
-                        <div className="edit-action">
-                            <img className="edit-icon" src='/images/icon-edit.svg'/>
-                            <p className="edit-text">Edit</p>
-                        </div>
-                        <div className="reply-action">
-                            <img className="reply-icon" src='/images/icon-reply.svg' />
-                            <p className="reply-text">Reply</p>
+            <div className='comment-section'>
+                <div className="comment-score">
+                    <img src="/images/icon-plus.svg" alt="" />
+                    <p className='comment-score-text'>{props.score}</p>
+                    <img src="/images/icon-minus.svg" alt="" />
+                </div>
+                <div className="comment-content">
+                    <div className="comment-header">
+                        <img className="comment-avatar" src={props.user.image.png}/>
+                        <p className="comment-author">{props.user.username}</p>
+                        {props.currentUser.username == props.user.username && <p className='self-tag'>you</p>}
+                        <p className="comment-time">{props.createdAt}</p>
+                        <div className="comment-actions">
+                           {props.currentUser.username == props.user.username ? <>
+                            <div className="delete-action">
+                                <img className="delete-icon" src='/images/icon-delete.svg' />
+                                <p className="delete-text">Delete</p>
+                            </div>
+                            <div className="edit-action" onClick={toggleIsEditing}>
+                                <img className="edit-icon" src='/images/icon-edit.svg'/>
+                                <p className="edit-text">Edit</p>
+                            </div>
+                            </>
+                             :
+                            <div className="reply-action"  onClick={toggleisReplying}>
+                                <img className="reply-icon" src='/images/icon-reply.svg' />
+                                <p className="reply-text">Reply</p>
+                            </div>}
                         </div>
                     </div>
+                    
+                    {isEditing ? 
+                    (<>
+                        <textarea rows={4} className="edit-comment-text" value={commentTextValue} name='commentText' onChange={handleChange}/>
+                        <button className='save-edit btn' onClick={()=>{props.updateComment(props.id, commentTextValue, props.isReply, props.parentId ); toggleIsEditing();}}>Update</button>
+                    </>
+                    )
+                    : (
+                    <div className="comment-text">
+                        {props.replyingTo && <span className='replyTo'>@{props.replyingTo},</span>} {props.content}
+                    </div>)
+                    }
                 </div>
-                <div className="comment-text">
-                    Impressive! Though it seems the drag feature could be improved. But overall it looks incredible. You've nailed the design and the responsiveness at various breakpoints works really well.
-                </div>
+                
             </div>
+            {isReplying && 
+                <div className='replying-section'>
+                    <img className="comment-avatar" src={props.currentUser.image.png}/>
+                    <textarea rows={4} className="reply-comment-text" value={replyingTextValue} onChange={handleReplyingTextValueChange}/>
+                    <button className='reply-edit btn' 
+                    onClick={()=>{props.handleReply(props.parentId, replyingTextValue, props.user.username); toggleisReplying()}}>Reply</button>
+                </div>
+            }
+        {props.replies?.length > 0 && (
+            <div className='replies-section'>
+                <div className='replies-divider'></div>
+                <div className='replies-content'>
+                    {props.replies.map(reply => <Comment key={reply.id} currentUser={props.currentUser} updateComment={props.updateComment} parentId={props.parentId} isReply={true} {...reply} handleReply={props.handleReply}/>)}
+                </div>
+            </div>) }
         </div>
+        </>
     )
 }
